b0808ee26a22216c7aacb4c0b6847dac2e3dadbc
bankushvij <bankushvij2977@gmail.com>
2022-08-19 13:44:40 +0530

    ccszffadg

--- build/index.js
+++ build/index.js
@@ -6,29 +6,29 @@ var _cors = _interopRequireDefault(require("cors"));
 
 var _helmet = _interopRequireDefault(require("helmet"));
 
-var _connections = _interopRequireDefault(require("./database/connections"));
+var _connections = _interopRequireDefault(require("./Database/connections.js"));
 
-var _google = _interopRequireDefault(require("./Config/google.config"));
+var _googleConfig = _interopRequireDefault(require("./Config/google.config.js"));
 
-var _route = _interopRequireDefault(require("./Config/route.config"));
+var _route = _interopRequireDefault(require("./Config/route.config,js"));
 
 var _passport = _interopRequireDefault(require("passport"));
 
-var _image = _interopRequireDefault(require("./API/image"));
+var _index = _interopRequireDefault(require("./API/image/index.js"));
 
-var _auth = _interopRequireDefault(require("./API/auth"));
+var _index2 = _interopRequireDefault(require("./API/auth/index.js"));
 
-var _restaurent = _interopRequireDefault(require("./API/restaurent"));
+var _index3 = _interopRequireDefault(require("./API/restaurent/index.js"));
 
-var _food = _interopRequireDefault(require("./API/food"));
+var _index4 = _interopRequireDefault(require("./API/food/index.js"));
 
-var _menu = _interopRequireDefault(require("./API/menu"));
+var _index5 = _interopRequireDefault(require("./API/menu/index.js"));
 
-var _review = _interopRequireDefault(require("./API/review"));
+var _index6 = _interopRequireDefault(require("./API/review/index.js"));
 
-var _user = _interopRequireDefault(require("./API/user"));
+var _index7 = _interopRequireDefault(require("./API/user/index.js"));
 
-var _order = _interopRequireDefault(require("./API/order"));
+var _index8 = _interopRequireDefault(require("./API/order/index.js"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
@@ -36,7 +36,7 @@ require("dotenv").config();
 
 var session = require('express-session');
 
-(0, _google["default"])(_passport["default"]);
+(0, _googleConfig["default"])(_passport["default"]);
 (0, _route["default"])(_passport["default"]);
 var zomato = (0, _express["default"])();
 zomato.use((0, _cors["default"])());
@@ -46,14 +46,14 @@ zomato.use(session({
   secret: 'ssshhhhh'
 }));
 zomato.use(_passport["default"].initialize());
-zomato.use("/api/auth", _auth["default"]);
-zomato.use("/api/image", _image["default"]);
-zomato.use("/api/restaurent", _restaurent["default"]);
-zomato.use("/api/food", _food["default"]);
-zomato.use("/api/menu", _menu["default"]);
-zomato.use("/api/order", _order["default"]);
-zomato.use("/api/review", _review["default"]);
-zomato.use("/api/user", _user["default"]);
+zomato.use("/api/auth", _index2["default"]);
+zomato.use("/api/image", _index["default"]);
+zomato.use("/api/restaurent", _index3["default"]);
+zomato.use("/api/food", _index4["default"]);
+zomato.use("/api/menu", _index5["default"]);
+zomato.use("/api/order", _index8["default"]);
+zomato.use("/api/review", _index6["default"]);
+zomato.use("/api/user", _index7["default"]);
 zomato.use(_passport["default"].session());
 var PORT = process.env.PORT || 4000;
 zomato.listen(PORT, function () {
